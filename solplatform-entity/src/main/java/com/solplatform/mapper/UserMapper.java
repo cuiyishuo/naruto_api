16b1f63d6ea8c06a33df50d36bb5f864dafe3b10
cuiyishuo <haizeier@gmail.com>
2020-01-05 15:13:42 +0800

    增加用户mapper类及用户sql；业务层增加注册业务；

--- solplatform-entity/src/main/java/com/solplatform/mapper/UserMapper.java
+++ solplatform-entity/src/main/java/com/solplatform/mapper/UserMapper.java
@@ -3,8 +3,13 @@ package com.solplatform.mapper;
 import com.solplatform.entity.UserEntity;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
+
 @Mapper
 public interface UserMapper {
     // 新增用户
-    public int addUser(UserEntity userEntity);
+    int addUser(UserEntity userEntity);
+    // 查询用户
+    List<UserEntity> selectUser();
 }
